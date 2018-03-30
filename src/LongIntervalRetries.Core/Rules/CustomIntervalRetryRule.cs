3932c667280956bcbe3f62fee5ae1b8941f4ae24
fangdong <fangdong@yodoo.net.cn>
2018-03-30 09:47:40 +0800

    提供默认的两种IRetryRule实现

---
+++ src/LongIntervalRetries.Core/Rules/CustomIntervalRetryRule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongIntervalRetries.Core.Rules
+{
+    /// <summary>
+    /// 自定义重试间隔的重试机制
+    /// </summary>
+    public class CustomIntervalRetryRule : IRetryRule
+    {
+        private TimeSpan[] _intervals;
+        /// <summary>
+        /// 默认构造实现,注意<see cref="GetNextFireSpan(int)"/>会按索引从intervals中获取对应TimeSpan，如未找到则返回TimeSpan.MinValue
+        /// </summary>
+        /// <param name="name">该RetryRule的唯一性名称</param>
+        /// <param name="intervals">自定义的时间间隔</param>
+        public CustomIntervalRetryRule(string name, params TimeSpan[] intervals)
+        {
+            if (intervals == null || intervals.Length == 0)
+            {
+                throw new ArgumentNullException("intervals can not be empty");
+            }
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("name can not be empty");
+            }
+            this._intervals = intervals;
+            this.Name = name;
+        }
+        /// <summary>
+        /// Get the name of the IRetryRule
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// 根据已经执行的次数获取下一次执行时间间隔，返回小于TimeSpan.Zero的值表示不再需要执行
+        /// </summary>
+        /// <param name="executedNumber"></param>
+        /// <returns></returns>
+        public TimeSpan GetNextFireSpan(int executedNumber)
+        {
+            if (executedNumber >= 0 && executedNumber < this._intervals.Length)
+            {
+                return this._intervals[executedNumber];
+            }
+            return TimeSpan.MinValue;
+        }
+    }
+}
---
+++ src/LongIntervalRetries.Core/Rules/SimpleRepeatRetryRule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongIntervalRetries.Core.Rules
+{
+    /// <summary>
+    /// 简单重复时间间隔重试机制
+    /// </summary>
+    public class SimpleRepeatRetryRule : IRetryRule
+    {
+        private int _maxExecutedNumber;
+        private TimeSpan _timeSpan;
+        /// <summary>
+        /// 默认构造实现
+        /// </summary>
+        /// <param name="maxExecutedNumber">允许的最大重试次数（含）</param>
+        /// <param name="timeSpan"></param>
+        public SimpleRepeatRetryRule(int maxExecutedNumber,TimeSpan timeSpan)
+        {
+            this._maxExecutedNumber = maxExecutedNumber;
+            this._timeSpan = timeSpan;
+        }
+        /// <summary>
+        /// Get the name of the IRetryRule
+        /// </summary>
+        public string Name => "LongIntervalRetries.SimpleRepeatRetryRule";
+        /// <summary>
+        /// 根据已经执行的次数获取下一次执行时间间隔，返回小于TimeSpan.Zero的值表示不再需要执行
+        /// </summary>
+        /// <param name="executedNumber"></param>
+        /// <returns></returns>
+        public TimeSpan GetNextFireSpan(int executedNumber)
+        {
+            if (executedNumber >= this._maxExecutedNumber)
+            {
+                return TimeSpan.MinValue;
+            }
+            return this._timeSpan;
+        }
+    }
+}
