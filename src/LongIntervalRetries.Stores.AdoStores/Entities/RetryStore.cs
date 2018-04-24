62de4d9d81312c8dabf2c1fc4314ca5e8c2973f6
fangdong <fangdong@yodoo.net.cn>
2018-04-24 14:54:15 +0800

    add adostores entities

---
+++ src/LongIntervalRetries.Stores.AdoStores/Entities/RetryStore.cs
@@ -0,0 +1,38 @@
+﻿using LongIntervalRetries.Rules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongIntervalRetries.Stores.AdoStores.Entities
+{
+    /// <summary>
+    /// 重试存储信息
+    /// </summary>
+    public class RetryStore
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// 当前Job状态，可以根据状态进行相应的业务处理
+        /// </summary>
+        public RetryJobStatus JobStatus { get; set; }
+        /// <summary>
+        /// 当前已执行次数
+        /// </summary>
+        public int ExecutedNumber { get; set; }
+        /// <summary>
+        /// 上一次执行时间，注意实际Job执行时间会在此基础上加上<see cref="IRetryRule.GetNextFireSpan(int)"/>时间
+        /// </summary>
+        public DateTime PreviousFireTime { get; set; }
+        /// <summary>
+        /// 完整的Job全名，包含程序集名称
+        /// </summary>
+        public string JobTypeName { get; set; }
+        /// <summary>
+        /// 要采用的<see cref="IRetryRule"/>规则，若不设定，则采用第一条规则
+        /// </summary>
+        public string UsedRuleName { get; set; }
+    }
+}
---
+++ src/LongIntervalRetries.Stores.AdoStores/Entities/RetryStoreData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongIntervalRetries.Stores.AdoStores.Entities
+{
+    /// <summary>
+    /// 重试相关的上下文数据信息
+    /// </summary>
+    public class RetryStoreData
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// <see cref="RetryStore.Id"/>
+        /// </summary>
+        public long RetryStoreId { get; set; }
+        /// <summary>
+        /// 键值名
+        /// </summary>
+        public string KeyName { get; set; }
+        /// <summary>
+        /// 序列化后的数据内容
+        /// </summary>
+        public string DataContent { get; set; }
+        /// <summary>
+        /// 完整的值类型全名，包含程序集名称
+        /// </summary>
+        public string DataTypeName { get; set; }
+    }
+}
